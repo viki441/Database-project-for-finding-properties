695a91e3b5ff186f140fd2cfb5f5b83ff168af4e
viki441 <viki.k.mitova@gmail.com>
2026-06-29 12:37:22 +0300

    first final state

--- backend/routes/main-form-request.js
+++ backend/routes/main-form-request.js
@@ -88,17 +88,24 @@ router.get("/statistics", (req, res) => {
 
 router.get("/agents", (req, res) => {
 
-    const sql = `
-    SELECT 
-        U.USERNAME,
-        U.NAMES,
-        A.WEBSITE,
+const sql = `
+    SELECT
+    U.USERNAME,
+    U.NAMES,
+    U.PHONENUMBER,
+    A.WEBSITE,
         COUNT(O.OFFERID) AS OFFER_COUNT
-    FROM FN3MI0700328.USERS U
-    JOIN FN3MI0700328.AGENCIES A ON A.USERNAME = U.USERNAME
-    LEFT JOIN FN3MI0700328.OFFERS O ON O.USERNAME = U.USERNAME
-    GROUP BY U.USERNAME, U.NAMES, A.WEBSITE
-    ORDER BY OFFER_COUNT DESC
+FROM FN3MI0700328.USERS U
+         JOIN FN3MI0700328.AGENCIES A
+              ON A.USERNAME = U.USERNAME
+         LEFT JOIN FN3MI0700328.OFFERS O
+                   ON O.USERNAME = U.USERNAME
+GROUP BY
+    U.USERNAME,
+    U.NAMES,
+    U.PHONENUMBER,
+    A.WEBSITE  
+ORDER BY OFFER_COUNT DESC
     FETCH FIRST 3 ROWS ONLY
 `;
 
@@ -115,5 +122,48 @@ router.get("/agents", (req, res) => {
     });
 });
 
+
+
+
+router.get("/property/:id", (req, res) => {
+
+    const id = req.params.id;
+
+
+    console.log("ID:", `"${id}"`);
+    console.log("Length:", id.length);
+
+
+    //
+
+    const sql = `
+    SELECT *
+FROM FN3MI0700328.OFFERS O
+JOIN FN3MI0700328.PROPERTIES P ON O.PROPERTYID = P.PROPERTYID
+JOIN FN3MI0700328.USERS U ON U.USERNAME = O.USERNAME
+JOIN FN3MI0700328.LOCATIONS L ON P.LOCATIONID = L.LOCATIONID
+JOIN FN3MI0700328.IMAGES I ON I.OFFERID = O.OFFERID
+LEFT JOIN FN3MI0700328.AGENCIES A ON U.USERNAME = A.USERNAME
+WHERE O.OFFERID = ?
+`;
+
+    ibmdb.open(connStr, (err, conn) => {
+        if (err) return res.status(500).json({ error: err.message });
+
+        conn.query(sql, [String(id)], (err, data) => {
+            conn.closeSync();
+
+            if (err) return res.status(500).json({ error: err.message });
+
+            console.log("RESULT:", data);
+
+            if (!data || data.length === 0) {
+                return res.status(404).json({ error: "Property not found" });
+            }
+
+            res.json(data[0] || null);
+        });
+    });
+});
 
 module.exports = router;
